b3e974a48b8762c4bb9c6a6b026f9c3c38b3e5cd
Ibragim <lol.3007@mail.ru>
2024-06-01 15:34:35 -0700

    fix for dep

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,15 +1,8 @@
-import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev';
 import { Provider } from 'react-redux';
 import { setupStore } from '../shared/store/store';
 import AppRoter from './router/AppRouter';
 
-import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from './provider/ThemeProvider';
-
-if (process.env.IS_DEV === 'TRUE') {
-  loadDevMessages();
-  loadErrorMessages();
-}
 
 const store = setupStore();
 function App() {
